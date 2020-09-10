5fd5c221e530404c2da7e00acb21da231f519521
Fawaz Ahmed <fawazahmed0@hotmail.com>
2020-09-10 16:16:11 +0530

    check python in other envs

---
+++ checkpy.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const {
+  spawnSync
+} = require('child_process');
+
+function runPyScript(pathToScript, args) {
+  // Using windows py to run python version 3
+  var output = spawnSync('py', ['-3', pathToScript].concat(args))
+  // Using python3 binary to run python version 3, if above fails
+  if (output.error)
+    output = spawnSync('python3', [pathToScript].concat(args))
+  // assuming python 3 is named as python in the system
+  if (output.error)
+    output = spawnSync('python', [pathToScript].concat(args))
+  if (output.error)
+    console.log("Either the translate script have failed or Python 3 might not be installed in the system")
+
+  return output.stdout.toString();
+}
+
+
+console.log(runPyScript(path.join(__dirname,'testscript.py')))
---
+++ testscript.py
@@ -0,0 +1,3 @@
+import sys
+print("Python version Hello world")
+print (sys.version)
